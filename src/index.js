a01223ef8d82a075b22ef91786c8fe11e5d23391
Cameron Soudbash <csoudbash@gmail.com>
2022-02-24 11:30:14 -0600

    first setup done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+
+const storeInstance = createStore(
+
+    // combineReducers(
+        // {
+
+        // }
+    // ),
+    applyMiddleware(
+        logger
+    )
+
+);
+
+ReactDOM.render(
+<Provider store= {storeInstance}>
+    <App />
+ </Provider>
+, document.getElementById('root'));
